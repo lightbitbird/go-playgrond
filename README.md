7ab809908cad7440b27e604124dbb8c193ee9df5
lightbitbird <maxgramf3@gmail.com>
2019-01-08 00:04:25 +0900

    Add rest api samples

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# go-playgrond
+# go-playground
 Go-lang playground: bassic syntax, slice, map, struct, type, function, method, interface, goroutine etc.
---
+++ rest/routing.go
@@ -0,0 +1,49 @@
+package main
+
+import ("net/http"
+    "github.com/julienschmidt/httprouter"
+    "fmt"
+    "github.com/gopub/log"
+)
+
+func main() {
+    router := httprouter.New()
+    router.GET("/", Index)
+    router.GET("/hello/:name", Hello2)
+    router.GET("/user/:uid", getuser)
+    router.POST("/adduser/:uid", adduser)
+    router.DELETE("/deleteuseer/:uid", deleteuser)
+    router.PUT("/modifyuser/:uid", modifyuser)
+
+    log.Fatal(http.ListenAndServe(":8099", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "welcom!\n")
+}
+
+func Hello2(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func getuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    uid := ps.ByName("uid")
+    fmt.Fprintf(w, "you are get user %s", uid)
+}
+
+func modifyuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    uid := ps.ByName("uid")
+    fmt.Fprintf(w, "you are modify user %s", uid)
+}
+
+func deleteuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    uid := ps.ByName("uid")
+    fmt.Fprintf(w, "you are delete user %s", uid)
+}
+
+func adduser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    // uid := r.FormValue("uid")
+    uid := ps.ByName("uid")
+    fmt.Fprintf(w, "you are add user %s", uid)
+}
+
